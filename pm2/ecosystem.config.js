e289cf9ce20db72c12bf9f9886a80407a2e15796
Pouya Eghbali <persian.writer@gmail.com>
2019-10-31 15:30:44 +0100

    Added load balancing

--- pm2/ecosystem.config.js
+++ pm2/ecosystem.config.js
@@ -1,18 +1,26 @@
 const settings = require('./settings.json');
 
+const app = {
+  script: 'bundle/main.js',
+  instances: 1,
+  autorestart: true,
+  watch: true,
+  exec_mode: "fork_mode",
+}
+
+const env = {
+  NODE_ENV: 'production',
+  MONGO_URL: 'mongodb://localhost:27017/{{database}}',
+  ROOT_URL: 'https://{{domain}}',
+  METEOR_SETTINGS: JSON.stringify(settings),
+}
+
 module.exports = {
   apps: [{
-    name: 'Ako',
-    script: 'bundle/main.js',
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    env: {
-      NODE_ENV: 'production',
-      MONGO_URL: 'mongodb://localhost:27017/{{database}}',
-      ROOT_URL: 'https://{{domain}}',
-      METEOR_SETTINGS: JSON.stringify(settings),
-      PORT: 3000
-    }
+    name: 'Ako-0', ...app,
+    env: { ...env, PORT: 3000 }
+  }, {
+    name: 'Ako-1', ...app,
+    env: { ...env, PORT: 3001 }
   }],
 };
